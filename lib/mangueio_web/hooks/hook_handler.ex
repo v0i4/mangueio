1993b6323f8b4aeed4cb70e8bc8a962b4ea87dfd
Antonio Vasconcellos <avsn.toni@gmail.com>
2024-04-10 12:18:12 -0300

    remove mix env

--- lib/mangueio_web/hooks/hook_handler.ex
+++ lib/mangueio_web/hooks/hook_handler.ex
@@ -6,11 +6,23 @@ defmodule MangueioWeb.HookHandler do
     # read some parameters from your env config
     env_config = Application.get_env(:mangueio, __MODULE__)
     # delete the webhook and set it again
-    {:ok, true} = Telegex.delete_webhook()
+    try do
+      {:ok, true} = Telegex.delete_webhook()
+    rescue
+      _ -> nil
+    end
+
     # set the webhook (url is required)
     # {:ok, true} = Telegex.set_webhook(env_config[:webhook_url])
-    {:ok, true} =
-      Telegex.set_webhook("https://eeba-2804-14d-bac2-870d-00-98b8.ngrok-free.app/api/telegram")
+
+    # if dev environment
+
+    if Mix.env() == :dev do
+      {:ok, true} =
+        Telegex.set_webhook("https://eeba-2804-14d-bac2-870d-00-98b8.ngrok-free.app/api/telegram")
+    else
+      Telegex.set_webhook("https://mangueio.fly.dev/api/telegram")
+    end
 
     %Telegex.Hook.Config{
       secret_token: env_config[:secret_token],
